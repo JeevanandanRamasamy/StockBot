c674b77304606618e917f978fd6518ca77c20439
jonar12 <jona.arredondo12@gmail.com>
2022-04-02 16:41:18 -0400

    implemented the database

--- stockbot.py
+++ stockbot.py
@@ -8,8 +8,15 @@ from pymongo import MongoClient
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+STARTINGBALANCE = 0
 
 bot = commands.Bot(command_prefix = '$')
+
+# Database setup
+cluster = MongoClient("mongodb+srv://stockbot:XLoy90B9uUYMCkZf@stockbot.t61sf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["stockbot"]
+collection = db["users"]
+
 #cluster = MongoClient('CONNECTION_URL')
 
 @bot.event
@@ -22,17 +29,37 @@ async def on_message(message):
         await message.channel.send('Testing 1 2 3!')
     await bot.process_commands(message)
 
-@bot.command(help = 'Returns your current balance')
+@bot.command(help = 'Set a new balance')
 async def setBalance(ctx, bal):
-    balance = bal
-    await ctx.send(f'Balance changed to ${bal}')
+    userId = ctx.message.author.id;
+    # If user already exists, locate the user and change the balance
+    user = collection.find_one({"_userID": userId})
+    if user:
+        collection.update_one({"_userID": userId}, {"$set": {"balance": bal}})
+    # If not create a post
+    else:
+        post = {"_userID": userId, "balance": bal}
+        await collection.insert_one(post);
+    
+    await ctx.send(f'Balance set to ${bal}')
 
 @bot.command(help = 'Returns your current balance')
 async def getBalance(ctx):
-    await ctx.send(balance)
+    userId = ctx.message.author.id
+    userName = ctx.message.author.name
+    # If the user exists, retrieve the user balance
+    user = collection.find_one({'_userID': userId})
+    if user:  # check if user exists
+        balance = user['balance']
+        msg = f'Your balance is: ${balance}'
+    else: 
+        msg = f'{userName}, set a balance first.'
+    await ctx.send(msg)
 
 @bot.command(help = 'Adds the stock to your account and subtracts cost from balance if you have sufficient funds')
 async def buy(ctx, num, stockName):
+    userId = ctx.message.author.id;
+    balance = collection.find_one({'_userID:': userId})['balance']
     if (balance >= num * stockName.getPrice()):
         balance -= num * stockName.getPrice()
     else:
@@ -44,5 +71,12 @@ async def sell(ctx, num, stockName):
         balance += num * stockName.getPrice()
     else:
         await ctx.channel.send(f'Unable to sell stock (You do not own {num} of this stock)')
+
+@bot.command(help = 'reset the balance') 
+async def reset(ctx):
+    userId = ctx.message.author.id;
+    # delete data from this user from the DB
+    collection.delete_one({"_userID" : userId})
+    await ctx.channel.send('Balance and operations were reset')
 
 bot.run(TOKEN)
