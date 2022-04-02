8514642cb9806a273d7d7d27d80eefb97de7afcb
Jeevanandan Ramasamy <94267209+JeevanandanRamasamy@users.noreply.github.com>
2022-04-02 16:40:42 -0400

    Add files via upload

--- stockbot.py
+++ stockbot.py
@@ -1,6 +1,8 @@
 import os
 import discord
 import pymongo
+import requests
+import json
 from dotenv import load_dotenv
 from discord.ext import commands
 from pymongo import MongoClient
@@ -10,7 +12,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix = '$')
-#cluster = MongoClient('CONNECTION_URL')
 
 @bot.event
 async def on_ready():
@@ -22,7 +23,7 @@ async def on_message(message):
         await message.channel.send('Testing 1 2 3!')
     await bot.process_commands(message)
 
-@bot.command(help = 'Returns your current balance')
+@bot.command(help = 'Changes your current balance to ')
 async def setBalance(ctx, bal):
     balance = bal
     await ctx.send(f'Balance changed to ${bal}')
@@ -32,17 +33,32 @@ async def getBalance(ctx):
     await ctx.send(balance)
 
 @bot.command(help = 'Adds the stock to your account and subtracts cost from balance if you have sufficient funds')
-async def buy(ctx, num, stockName):
-    if (balance >= num * stockName.getPrice()):
-        balance -= num * stockName.getPrice()
+async def buy(ctx, num, stock):
+    balance = 10000.0
+    response = requests.get(f'https://api.twelvedata.com/price?symbol={stock}&apikey=a8b0b60da8d84235a0da19805b2552f3')
+    price = float(json.loads(response.text)['price'])
+    if balance >= num * price:
+        balance -= num * price
     else:
-        await ctx.channel.send('Unable to buy stock (Not enough funds)')
+        await ctx.send('Unable to buy stock (Not enough funds)')
 
 @bot.command(help = 'Removes the stock from your account and adds current stock price to balance if you own the stock')
-async def sell(ctx, num, stockName):
-    if (num <= user.stock.amount):
-        balance += num * stockName.getPrice()
+async def sell(ctx, num, stock):
+    response = requests.get(f'https://api.twelvedata.com/price?symbol={stock}&apikey=a8b0b60da8d84235a0da19805b2552f3')
+    if num <= user.stock.amount:
+        balance += num * float(response.text)
     else:
-        await ctx.channel.send(f'Unable to sell stock (You do not own {num} of this stock)')
+        await ctx.send(f'Unable to sell stock (You do not own {num} of this stock)')
+
+@bot.command(help = 'Lists some stocks that you can buy')
+async def listStocks(ctx):
+    response = requests.get('https://api.twelvedata.com/stocks')
+    symbols = []
+    for stock in json.loads(response.text)['data']:
+        if stock['country'] == 'United States' and stock['currency'] == 'USD':
+            symbols.append(stock['symbol'])
+    for i in range(20):
+        await ctx.send(symbols[i])
+        print(symbols[i])
 
 bot.run(TOKEN)
